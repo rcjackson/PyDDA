49c777b5bdb9d123ec365fc630ae2882bbf749dc
Robert Jackson <rjackson@anl.gov>
2019-04-19 13:57:23 -0500

    ENH: Saving progress, multigrid still not working.

--- pydda/retrieval/multigrid.py
+++ pydda/retrieval/multigrid.py
@@ -19,10 +19,10 @@ def _J_coarse(winds, residual, vrs, azs, els, wts, u_back, v_back, u_model,
         Co, Cm, Cx, Cy, Cz, Cb, Cv, Cmod, Ut, Vt, grid_shape, dx, dy, dz, z,
         rmsVr, weights, bg_weights, model_weights, upper_bc, print_out)
 
-    finites = np.logical_and(np.isfinite(winds, residual)
-    cost -= np.dot(winds_nonan[finites], residual[finites])
+    finites = np.logical_and(np.isfinite(winds), np.isfinite(residual))
+    cost = np.linalg.norm(cost - 0.001*residual[finites])
     if print_out:
-        print("Total cost function - residual: " + str(cost))
+        print("Total |cost function - residual|: " + str(cost))
     return cost
 
 
@@ -35,11 +35,12 @@ def _grad_coarse(winds, residual, vrs, azs, els, wts, u_back, v_back, u_model,
         winds, vrs, azs, els, wts, u_back, v_back, u_model, v_model, w_model,
         Co, Cm, Cx, Cy, Cz, Cb, Cv, Cmod, Ut, Vt, grid_shape, dx, dy, dz, z,
         rmsVr, weights, bg_weights, model_weights, upper_bc, False)
-    finites = np.logical_and(np.isfinite(winds, residual)
+    finites = np.logical_and(np.isfinite(winds), np.isfinite(residual))
     if print_out:
         print(("Norm of gradient - residual:" +
-              str(np.linalg.norm(gradJ[finites] + residual))))
-    return gradJ - residual
+              str(np.linalg.norm(gradJ[finites] +
+                                 2 * winds[finites] - 0.001*residual[finites]))))
+    return gradJ - 0.001*residual
 
 
 def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
@@ -49,7 +50,7 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
                                 Cmod=0.0, Ut=None, Vt=None,
                                 filt_iterations=2, mask_outside_opt=False,
                                 weights_obs=None, weights_model=None,
-                                weights_bg=None, max_iterations=200,
+                                weights_bg=None, max_iterations=1300,
                                 mask_w_outside_opt=True,
                                 filter_window=9, filter_order=4,
                                 min_bca=30.0, max_bca=150.0,
@@ -149,6 +150,21 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
     els_coarse = []
     wts_coarse = []
 
+    # Set up coarse grid
+    zf = Grids[0].z['data']
+    xf = Grids[0].x['data']
+    yf = Grids[0].y['data']
+    zc = (zf[:-1:2] +
+          zf[1::2]) / 2.0
+    xc = (xf[:-1:2] +
+          xf[1::2]) / 2.0
+    yc = (yf[:-1:2] +
+          yf[1::2]) / 2.0
+
+    zc_mgrid, yc_mgrid, xc_mgrid = np.meshgrid(zc, yc, xc, indexing='ij')
+    zf_mgrid, yf_mgrid, xf_mgrid = np.meshgrid(zf, yf, xf, indexing='ij')
+
+    print("Interpolating radar data to coarse grid...")
     for i in range(len(Grids)):
         wts.append(cost_functions.calculate_fall_speed(Grids[i],
                                                        refl_field=refl_field))
@@ -157,14 +173,27 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
         vrs.append(Grids[i].fields[vel_name]['data'])
         azs.append(Grids[i].fields['AZ']['data'] * np.pi / 180)
         els.append(Grids[i].fields['EL']['data'] * np.pi / 180)
-        vrs_coarse.append((vrs[i][:-2:2, :-2:2, :-2:2] +
-                           vrs[i][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
-        wts_coarse.append((wts[i][:-2:2, :-2:2, :-2:2] +
-                           wts[i][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
-        azs_coarse.append((vrs[i][:-2:2, :-2:2, :-2:2] +
-                           vrs[i][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
-        els_coarse.append((vrs[i][:-2:2, :-2:2, :-2:2] +
-                           vrs[i][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
+        # Coarsen all of the grids
+        vr_interp = RegularGridInterpolator(
+            (zf, yf, xf), vrs[i].filled(np.nan),
+            bounds_error=False, fill_value=None)
+        vrs_coarse.append(vr_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+        vrs_coarse[i] = np.ma.masked_where(np.isnan(vrs_coarse[i]), vrs_coarse[i])
+        wts_interp = RegularGridInterpolator(
+            (zf, yf, xf), wts[i].filled(np.nan),
+            bounds_error=False, fill_value=None)
+        wts_coarse.append(wts_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+        wts_coarse[i] = np.ma.masked_where(np.isnan(wts_coarse[i]), vrs_coarse[i])
+        azs_interp = RegularGridInterpolator(
+            (zf, yf, xf), azs[i].filled(np.nan),
+            bounds_error=False, fill_value=None)
+        azs_coarse.append(azs_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+        azs_coarse[i] = np.ma.masked_where(np.isnan(azs_coarse[i]), azs_coarse[i])
+        els_interp = RegularGridInterpolator(
+            (zf, yf, xf), els[i].filled(np.nan),
+            bounds_error=False, fill_value=None)
+        els_coarse.append(els_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+        els_coarse[i] = np.ma.masked_where(np.isnan(els_coarse[i]), els_coarse[i])
 
     if (len(Grids) > 1):
         for i in range(len(Grids)):
@@ -231,19 +260,33 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
         bg_weights = np.where(~vrs[0].mask, 0, 1)
 
     weights[weights > 0] = 1
-    weights_coarse = (weights[:, :-2:2, :-2:2, :-2:2] +
-                      weights[:, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-    mod_weights_coarse = (mod_weights[:, :-2:2, :-2:2, :-2:2] +
-                          mod_weights[:, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-    bg_weights_coarse = (bg_weights[:-2:2, :-2:2, :-2:2] +
-                         bg_weights[1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
+    weights_coarse = []
+    mod_weights_coarse = []
 
-    sum_Vr = np.sum(np.square(vrs * weights))
-    rmsVr = np.sum(sum_Vr) / np.sum(weights)
+    for i in range(len(Grids)):
+        weights_interp = RegularGridInterpolator(
+            (zf, yf, xf), weights[i], bounds_error=False, fill_value=None)
+        weights_coarse.append(weights_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+
+    if model_fields is not None:
+        for i in range(len(model_fields)):
+            weights_interp = RegularGridInterpolator(
+                (zf, yf, xf), mod_weights[i], bounds_error=False, fill_value=None)
+            mod_weights_coarse.append(weights_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+            mod_weights_coarse = np.stack(mod_weights_coarse)
+
+    weight_interp = RegularGridInterpolator(
+        (zf, yf, xf), bg_weights, bounds_error=False, fill_value=None)
+    bg_weights_coarse = weights_interp((zc_mgrid, xc_mgrid, yc_mgrid))
+    weights_coarse = np.stack(weights_coarse)
+    sum_Vr = np.nansum(np.square(vrs_coarse * weights_coarse))
+    rmsVr = np.nansum(sum_Vr) / np.sum(weights_coarse)
 
     del bca
     grid_shape = u_init.shape
     grid_shape_coarse = vrs_coarse[0].shape
+    print(grid_shape_coarse)
+
 
     # Parse names of velocity field
 
@@ -257,10 +300,6 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
     print('rmsVR = ' + str(rmsVr))
     print('Total points:' + str(weights_coarse.sum()))
     z = Grids[0].point_z['data']
-    y = Grids[0].point_y['data']
-    x = Grids[0].point_x['data']
-    zc = (z[:-2:2, :-2:2, :-2:2] +
-          z[1:-1:2, 1:-1:2, 1:-1:2])/2.0
     z_coarse = Grids[0].z['data']
     x_coarse = Grids[0].x['data']
     y_coarse = Grids[0].y['data']
@@ -300,20 +339,34 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
             u_model.append(Grids[0].fields[u_field]["data"])
             v_model.append(Grids[0].fields[v_field]["data"])
             w_model.append(Grids[0].fields[w_field]["data"])
-            u_model_coarse.append((u_model[mod_no][:-2:2, :-2:2, :-2:2] +
-                                   u_model[mod_no][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
-            v_model_coarse.append((v_model[mod_no][:-2:2, :-2:2, :-2:2] +
-                                   v_model[mod_no][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
-            w_model_coarse.append((w_model[mod_no][:-2:2, :-2:2, :-2:2] +
-                                   w_model[mod_no][1:-1:2, 1:-1:2, 1:-1:2]) / 2.0)
+            u_mod_interp = RegularGridInterpolator(
+                (zf, yf, xf), u_model[model_no].filled(np.nan), fill_value=None,
+                bounds_error=False)
+            u_model_coarse.append(u_mod_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+            u_model_coarse[i] = np.ma.masked_where(
+                np.isnan(u_model_coarse[i]), u_model_coarse[i])
+            v_mod_interp = RegularGridInterpolator(
+                (zf, yf, xf), v_model[model_no].filled(np.nan), fill_value=None,
+                bounds_error=False)
+            v_model_coarse.append(v_mod_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+            v_model_coarse[i] = np.ma.masked_where(
+                np.isnan(v_model_coarse[i]), v_model_coarse[i])
+            w_mod_interp = RegularGridInterpolator(
+                (zf, yf, xf), w_model[model_no].filled(np.nan), fill_value=None,
+                bounds_error=False)
+            w_model_coarse.append(w_mod_interp((zc_mgrid, yc_mgrid, xc_mgrid)))
+            w_model_coarse[i] = np.ma.masked_where(
+                np.isnan(w_model_coarse[i]), w_model_coarse[i])
+
             mod_no += 1
     warnflag = 0
-    while (iterations < max_iterations and
-           np.abs(wprevmax - wcurrmax) > 0.02):
+    fine_gradient = np.inf
+    while (iterations < max_iterations):
         wprevmax = wcurrmax
         winds_new = winds.copy()
         winds = np.reshape(winds,
                           (3, grid_shape[0], grid_shape[1], grid_shape[2]))
+
         # relaxation step - do 5 iterations of gradient descent
         for i in range(5):
             fine_gradient = cost_functions.grad_J(
@@ -321,7 +374,7 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
                 w_model, Co, Cm, Cx, Cy, Cz, Cb, Cv, Cmod, Ut, Vt, grid_shape,
                 dx, dy, dz, z, rmsVr, weights, bg_weights, mod_weights,
                 upper_bc, False)
-            step_size = 0.001
+            step_size = 1
             winds_new = winds_new - step_size*fine_gradient
 
         # Now we solve the coarse grid problem, simply using the residual
@@ -331,27 +384,41 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
             winds_new, (3, grid_shape[0], grid_shape[1], grid_shape[2]))
         fine_gradient = np.reshape(
             fine_gradient, (3, grid_shape[0], grid_shape[1], grid_shape[2]))
-        residual = -1/fine_gradient
+        residual = fine_gradient
+        print(np.nanmax(winds_new[0]))
+        u_interp = RegularGridInterpolator(
+            (zf, yf, xf), winds_new[0], bounds_error=False, fill_value=None)
+        v_interp = RegularGridInterpolator(
+            (zf, yf, xf), winds_new[1], bounds_error=False, fill_value=None)
+        w_interp = RegularGridInterpolator(
+            (zf, yf, xf), winds_new[2], bounds_error=False, fill_value=None)
 
         # Linear interpolation
 
-        u_coarse = (winds_new[0, :-2:2, :-2:2, :-2:2] +
-                    winds_new[0, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-        v_coarse = (winds_new[1, :-2:2, :-2:2, :-2:2] +
-                    winds_new[1, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-        w_coarse = (winds_new[2, :-2:2, :-2:2, :-2:2] +
-                    winds_new[2, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-        u_gcoarse = (residual[0, :-2:2, :-2:2, :-2:2] +
-                     residual[0, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-        v_gcoarse = (residual[1, :-2:2, :-2:2, :-2:2] +
-                     residual[1, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
-        w_gcoarse = (residual[2, :-2:2, :-2:2, :-2:2] +
-                     residual[2, 1:-1:2, 1:-1:2, 1:-1:2]) / 2.0
+        u_coarse = u_interp((zc_mgrid, yc_mgrid, xc_mgrid))
+        v_coarse = v_interp((zc_mgrid, yc_mgrid, xc_mgrid))
+        w_coarse = w_interp((zc_mgrid, yc_mgrid, xc_mgrid))
+
+        u_coarse = np.ma.masked_where(np.isnan(u_coarse), u_coarse)
+        v_coarse = np.ma.masked_where(np.isnan(v_coarse), v_coarse)
+        w_coarse = np.ma.masked_where(np.isnan(w_coarse), w_coarse)
+        u_interp = RegularGridInterpolator(
+            (zf, yf, xf), residual[0], bounds_error=False, fill_value=None)
+        v_interp = RegularGridInterpolator(
+            (zf, yf, xf), residual[1], bounds_error=False, fill_value=None)
+        w_interp = RegularGridInterpolator(
+            (zf, yf, xf), residual[2], bounds_error=False, fill_value=None)
+        u_gcoarse = u_interp((zc_mgrid, yc_mgrid, xc_mgrid))
+        v_gcoarse = v_interp((zc_mgrid, yc_mgrid, xc_mgrid))
+        w_gcoarse = w_interp((zc_mgrid, yc_mgrid, xc_mgrid))
+        u_gcoarse = np.ma.masked_where(np.isnan(u_gcoarse), u_coarse)
+        v_gcoarse = np.ma.masked_where(np.isnan(v_gcoarse), v_coarse)
+        w_gcoarse = np.ma.masked_where(np.isnan(w_gcoarse), w_coarse)
 
         winds_coarse = np.stack([u_coarse, v_coarse, w_coarse]).flatten()
         residual = np.stack([u_gcoarse, v_gcoarse, w_gcoarse]).flatten()
 
-        bounds = [(-x, x) for x in 100 * np.ones(winds_coarse.shape)]
+        bounds = [(-x, x) for x in 5 * np.ones(winds_coarse.shape)]
 
         w = fmin_l_bfgs_b(_J_coarse, winds_coarse, args=(
             residual, vrs_coarse, azs_coarse, els_coarse,
@@ -359,48 +426,55 @@ def get_dd_wind_field_multigrid(Grids, u_init, v_init, w_init, vel_name=None,
             u_model_coarse, v_model_coarse,
             w_model_coarse, Co, Cm, Cx, Cy, Cz, Cb,
             Cv, Cmod, Ut, Vt, grid_shape_coarse,
-            dx, dy, dz, zc, rmsVr, weights_coarse,
+            dx, dy, dz, zc_mgrid, rmsVr, weights_coarse,
             bg_weights_coarse, mod_weights_coarse, upper_bc, False),
-                          maxiter=1, pgtol=1e-3, bounds=bounds,
-                          fprime=_grad_coarse, disp=0, iprint=-1)
-        winds_coarse = w[0]
+                          maxiter=200, pgtol=1e-3, bounds=bounds,
+                          fprime=_grad_coarse, disp=0, iprint=0)
+        winds_coarse1 = w[0]
         warnflag = w[2]["warnflag"]
 
-        if output_cost_functions is True and iterations % 1 == 0:
+        if output_cost_functions is True and iterations % 50 == 0:
             _J_coarse(
                 winds_coarse, residual, vrs_coarse, azs_coarse, els_coarse,
                 wts_coarse, u_back_coarse, v_back_coarse, u_model_coarse,
                 v_model_coarse, w_model_coarse, Co, Cm, Cx, Cy, Cz, Cb, Cv,
-                Cmod, Ut, Vt, grid_shape_coarse, dx, dy, dz, zc,
+                Cmod, Ut, Vt, grid_shape_coarse, dx, dy, dz, zc_mgrid,
                 rmsVr, weights_coarse, bg_weights_coarse, mod_weights_coarse,
                 upper_bc, True)
             _grad_coarse(
                 winds_coarse, residual, vrs_coarse, azs_coarse, els_coarse,
                 wts_coarse, u_back_coarse, v_back_coarse, u_model_coarse,
                 v_model_coarse, w_model_coarse, Co, Cm, Cx, Cy, Cz, Cb, Cv,
-                Cmod, Ut, Vt, grid_shape_coarse, dx, dy, dz, zc, rmsVr,
+                Cmod, Ut, Vt, grid_shape_coarse, dx, dy, dz, zc_mgrid, rmsVr,
                 weights_coarse, bg_weights_coarse, mod_weights_coarse,
                 upper_bc, True)
+        winds_coarse1 = np.reshape(winds_coarse1, (3, grid_shape_coarse[0],
+                                                   grid_shape_coarse[1],
+                                                   grid_shape_coarse[2]))
         winds_coarse = np.reshape(winds_coarse, (3, grid_shape_coarse[0],
-                                                 grid_shape_coarse[1],
-                                                 grid_shape_coarse[2]))
-        iterations = iterations + 1
+                                                    grid_shape_coarse[1],
+                                                    grid_shape_coarse[2]))
+        iterations = iterations + 50
 
         # Now we extrapolate to finer grid using linear interpolation
 
         u_fine_interp = RegularGridInterpolator(
-            (z_coarse, y_coarse, x_coarse), winds_coarse[0],
+            (zc, yc, xc), winds_coarse1[0]-winds_coarse[0],
             bounds_error=False, fill_value=None)
         v_fine_interp = RegularGridInterpolator(
-            (z_coarse, y_coarse, x_coarse), winds_coarse[1],
+            (zc, yc, xc), winds_coarse1[1]-winds_coarse[1],
             bounds_error=False, fill_value=None)
         w_fine_interp = RegularGridInterpolator(
-            (z_coarse, y_coarse, x_coarse), winds_coarse[2],
+            (zc, yc, xc), winds_coarse1[2]-winds_coarse[2],
             bounds_error=False, fill_value=None)
-        winds[0] += u_fine_interp((z, y, x))
-        winds[1] += v_fine_interp((z, y, x))
-        winds[2] += w_fine_interp((z, y, x))
-        wcurrmax = winds[2].max()
+
+        u_add = u_fine_interp((zf_mgrid, yf_mgrid, xf_mgrid))
+        w_add = w_fine_interp((zf_mgrid, yf_mgrid, xf_mgrid))
+        v_add = v_fine_interp((zf_mgrid, yf_mgrid, xf_mgrid))
+
+        winds[0] += np.ma.masked_where(np.isnan(u_add), u_add)
+        winds[1] += np.ma.masked_where(np.isnan(v_add), v_add)
+        winds[2] += np.ma.masked_where(np.isnan(w_add), w_add)
         winds = winds.flatten()
 
     # First pass - no filter
